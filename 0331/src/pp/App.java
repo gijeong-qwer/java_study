fdb4809d90ac82a60ee8c2f98b44f71402b1ef92
gijeong-qwer <gijeong@example.com>
2025-03-31 17:04:59 +0900

    전체 변경 사항 업데이트

--- 0331/src/pp/App.java
+++ 0331/src/pp/App.java
@@ -14,6 +14,22 @@ public class App {
 
         System.out.println(map.get("apple")); // 7
         System.out.println(map.containsKey("banana")); // true
+
+        Map<String, Object> map2 = new HashMap<>();
+
+        map2.put("studentInfo", new Student("magenta"));
+        map2.put("hobby", "축구");
+        map2.put("...", 1);
+
+        map2.get("studentInfo");
+        // Student aa = map.get("studentInfo"); >> 넣을때는 맘대로 넣을수 있지만 가져올때는 마음대로 못자겨옴
+        Student aa = (Student) map2.get("studentInfo"); // 그래서 타입 캐스팅을 해줘야함
+
+        System.out.println(map2.get("studentInfo")); // toString사용하면됨 >>
+        System.out.println(aa.getName());
+        System.out.println(map2.get("hobby"));
+        System.out.println(map2.get("magenta")); // null >> key값을 기준으로 검색하는거라 key값이 없어서
+        // 이 4개를 엮으면 난이도가 확오름
     }
 }
 
@@ -34,4 +50,29 @@ class HTMLparse implements Parseable {
     public void Parse() {
         System.out.println("HTML ");
     }
+}
+
+class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // @Override
+    // public String toString() {
+    // // TODO Auto-generated method stub
+    // return super.toString();
+    // }
+
+    @Override
+    public String toString() {
+
+        return name;
+    }
+
 }
